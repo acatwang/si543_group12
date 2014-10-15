5d85351df120004aa0a05d4b5b9630a161642862
kushankr <kushankr@umich.edu>
2014-10-14 21:51:50 -0400

    Added Home Page, Login Page, Facebook SDK-kushankr
    Added Home page
    Added Login/Register Page
    Added Facebook SDK for FB signin
    Modified Manifest, Gradle to sync with FB and Google+

---
+++ ConnectMentor/app/src/main/java/com/example/user/connectmentor/UserLoginActivity.java
@@ -0,0 +1,82 @@
+/*Kushank Raghav*/
+package com.example.user.connectmentor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+import com.facebook.*;
+import com.facebook.model.*;
+
+
+public class UserLoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.user_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void loginfb(View view) {
+         //start Facebook Authentication
+         //check for authentication state changes
+         //result of authentication
+         //if result == success
+           // Login user, use Intent to switch to next Activity -> OverView Activity
+
+
+    }
+    public void logingoogle(View view) {
+        //start Google Authentication
+        //check for authentication state changes
+        //retrieve result of authentication
+        //if result == success
+          // Login user, use Intent to switch to next Activity -> OverView Activity
+
+
+    }
+    public void signin(View view) {
+        //Retrieve values from text fields
+        //store them in variables-username,password
+        //Connect to Database
+        //Check if the username already exists in database by Select statement with where clause
+         //if the username exists
+           //retrieve corresponding password from db by Select statement with where clause
+           //if retrieved password == password
+              // Login the user,use Intent to switch to next Activity -> OverView Activity
+           //if retrieved password != password
+              // Do Nothing (stay on same activity)
+         //if the username does not exist
+           // insert username in db
+           //insert password in db
+           //Login the user, use Intent to switch to next Activity -> OverView Activity
+
+
+
+    }
+
+    }
+
