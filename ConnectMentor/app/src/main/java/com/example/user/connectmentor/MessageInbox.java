b50e44e9b5df1b086b03672818ec984488991ad0
ericayc <ericayc@umich.edu>
2014-10-15 14:58:02 -0400

    Update

--- ConnectMentor/app/src/main/java/com/example/user/connectmentor/MessageInbox.java
+++ ConnectMentor/app/src/main/java/com/example/user/connectmentor/MessageInbox.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MyActivity2 extends Activity {
+public class MessageInbox extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_activity2);
+        setContentView(R.layout.activity_message_inbox);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_activity2, menu);
+        getMenuInflater().inflate(R.menu.message_inbox, menu);
         return true;
     }
 
@@ -34,6 +34,7 @@ public class MyActivity2 extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
 
 //Messages show in a list view when received
 //Name of the person sent from
--- ConnectMentor/app/src/main/java/com/example/user/connectmentor/Settings.java
+++ ConnectMentor/app/src/main/java/com/example/user/connectmentor/Settings.java
@@ -6,18 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MyActivity extends Activity {
+public class Settings extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_settings);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
 
